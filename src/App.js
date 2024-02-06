706122bd4e491705a3f9058f587b7379c41b4535
nattallik77 <91556919+nattallik77@users.noreply.github.com>
2024-02-06 18:51:04 +0200

    convert static pages to dynamic react components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
+
+import React from 'react';
+// eslint-disable-next-line no-unused-vars
+import { useEffect } from 'react';
+import { BrowserRouter, Routes} from "react-router-dom";
+// eslint-disable-next-line no-unused-vars
+import { Route } from "react-router-dom";
+import Navbar from './Components/Navbar/Navbar';
+import Layout from './Components/Landing_Page/LandingPage';
+
+// eslint-disable-next-line no-unused-vars
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+          <Navbar/>
+              <Routes>
+              <Route path="/" element={<Layout/>}/>
+             
+              {/*<Route path="/Login" element={<Login/>}/>
+              <Route path="/SignUp" element={<SignUp/>}/>
+// eslint-disable-next-line no-unused-vars
+import Login from './Components/Login/Login';
+// eslint-disable-next-line no-unused-vars
+import SignUp from './Components/Sign_Up/Sign_Up';
+ <Route path="/Home" element={<Layout/>}/>
+              import Home from './Components/Landing_Page/Landing_page';
+              */}
+              
+              </Routes>
+            
+        </BrowserRouter>
+       
     </div>
   );
 }
